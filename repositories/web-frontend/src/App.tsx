c039649375f98c50896db2d339ebad3710f45ecc
Maece Fischer <contact@maecefischer.ch>
2025-06-16 21:18:31 +0200

    fix shadcn with tailwind config

--- repositories/web-frontend/src/App.tsx
+++ repositories/web-frontend/src/App.tsx
@@ -1,7 +1,28 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card.tsx'
+
 function App() {
   return (
     <>
       <h1 className="text-red-500">Foo</h1>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </>
   )
 }
